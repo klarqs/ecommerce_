1b31a78350b7a857cf0d7f0adc29fd66f75565df
paulklart <paulkolawole24@yahoo.com>
2020-09-28 04:15:47 +0100

    eCommerce

---
+++ shop/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+def shop(request):
+    context = {}
+    return render(request, 'shop/shop.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'shop/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'shop/checkout.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'shop/login.html', context)
+
